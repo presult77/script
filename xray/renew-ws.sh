4f9b2689d671ff06d773a25f40e1682793d04d5a
Ridho <presult77@gmail.com>
2022-10-29 15:19:48 +0700

    fix-renew

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ==========================================
+# ━━━━━━━━━━━━━━━━━━━━━===========
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -9,7 +9,7 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-# ==========================================
+# ━━━━━━━━━━━━━━━━━━━━━===========
 # Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
@@ -37,12 +37,12 @@ clear
 
 	clear
 	echo "" | tee -a /etc/log-create-user.log
-	echo "===============================" | tee -a /etc/log-create-user.log
+	echo "━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 	echo "  Xray/Vmess Account Renewed  " | tee -a /etc/log-create-user.log
-	echo "===============================" | tee -a /etc/log-create-user.log
+	echo "━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 	echo "Username  : $user" | tee -a /etc/log-create-user.log
 	echo "Expired   : $exp4" | tee -a /etc/log-create-user.log
-	echo "===============================" | tee -a /etc/log-create-user.log
+	echo "━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 	echo "THANKS FOR USING OUR SERVICE" | tee -a /etc/log-create-user.log
 
 	sleep 1
