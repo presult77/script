b0dc17de088b110fcfec256eabb4349d176951c4
Ridho <presult77@gmail.com>
2022-11-12 13:07:12 +0700

    update del

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -13,8 +13,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
 		echo "You have no existing clients!"
 		echo ""
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		read -n 1 -s -r -p "Press any key to back on menu"
-        menu
 	fi
 
 	clear
--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -18,16 +18,30 @@ MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		exit
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\E[44;1;39m     ⇱ Delete Trojan Account ⇲     \E[0m"
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo ""
+		echo "You have no existing clients!"
+		echo ""
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	fi
 
+	clear
+	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[44;1;39m     ⇱ Delete Trojan Account ⇲     \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo "  User       Expired  " 
+	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+    echo ""
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
-    if [ -z $user ]; then
-    exit
-    else
-
+    CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+    if [[ ${CLIENT_EXISTS} == '0' ]]; then
+    exit 1
+	else
 	exp=$(grep -E "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-	sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 	sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 	rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json
 
