64338cd9ebaae1a50e9e4e08d1ad73f3c9553da9
Ridho <presult77@gmail.com>
2022-11-27 21:13:22 +0700

    statusvm

---
+++ menu/status-vmess.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+# Getting
+# IP Validation
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+MYIP=$(curl -sS ipinfo.io/ip)
+
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+clear
+
+# GETTING OS INFORMATION
+source /etc/os-release
+Versi_OS=$VERSION
+ver=$VERSION_ID
+Tipe=$NAME
+URL_SUPPORT=$HOME_URL
+basedong=$ID
+
+# VPS ISP INFORMATION
+#ITAM='\033[0;30m'
+echo -e "$ITAM"
+REGION=$( curl -s ipinfo.io/region )
+CITY=$( curl -s ipinfo.io/city )
+tls_v2ray_status=$(systemctl status xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+nontls_v2ray_status=$(systemctl status xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+
+# STATUS SERVICE  TLS 
+if [[ $tls_v2ray_status == "running" ]]; then 
+   status_tls_v2ray=" ${GREEN}Running${NC} ( No Error )"
+else
+   status_tls_v2ray="${RED}  Not Running${NC}   ( Error )"
+fi
+
+# STATUS SERVICE NON TLS V2RAY
+if [[ $nontls_v2ray_status == "running" ]]; then 
+   status_nontls_v2ray=" ${GREEN}Running ${NC}( No Error )${NC}"
+else
+   status_nontls_v2ray="${RED}  Not Running ${NC}  ( Error )${NC}"
+fi
+
+# OS UPTIME
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+
+# WAKTU SEKARANG 
+harini=`date -d "0 days" +"%d-%m-%Y"`
+jam=`date -d "0 days" +"%X"`
+
+# GETTING DOMAIN NAME
+Domen="$(cat /etc/xray/domain)"
+echo -e ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ VMess Service Status ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "Time                    : $harini, $jam"
+echo -e "Server                  : $Domen"
+echo -e "Uptime                  : $uptime"
+echo -e "XRAYS Vmess TLS         :$status_tls_v2ray"
+echo -e "XRAYS Vmess None TLS    :$status_nontls_v2ray"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo ""
