d21b60b45f3db627803e184f70e3b828566bf7ca
Ridho <presult77@gmail.com>
2022-12-21 13:30:10 +0700

    addvmess

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -27,10 +27,10 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmess$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user-ws""'"' /etc/xray/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user-grpc""'"' /etc/xray/config.json
 acs=`cat<<EOF
       {
       "v": "2",
@@ -85,7 +85,7 @@ worry=`cat<<EOF
       "v": "2",
       "ps": "${user}",
       "add": "${user}.${domain}",
-      "port": "80",
+      "port": "8080",
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
@@ -110,8 +110,8 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━" | tee 
 echo -e "Remarks        : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-user.log
 echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-user.log
-echo -e "Port none TLS  : ${none}" | tee -a /etc/log-create-user.log
-echo -e "Port  GRPC     : ${tls}" | tee -a /etc/log-create-user.log
+echo -e "Port none TLS  : ${none}, 8080" | tee -a /etc/log-create-user.log
+echo -e "Port GRPC      : ${tls}" | tee -a /etc/log-create-user.log
 echo -e "id             : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "alterId        : 0" | tee -a /etc/log-create-user.log
 echo -e "Security       : auto" | tee -a /etc/log-create-user.log
