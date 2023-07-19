1fe746849d63c49b6b4702e665b6df66917be32e
RidF <presult77@gmail.com>
2023-07-19 20:44:26 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -50,6 +50,10 @@ cat > /etc/rc.local <<-END
 exit 0
 END
 
+#disableipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
 # Ubah izin akses
 chmod +x /etc/rc.local
 
