819c872246d5a049bc3d2d24609c972f9339038b
presult77 <presut77@gmail.com>
2023-08-25 20:56:04 +0700

    addtrojan

---
+++ xray/buat-trojan.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+
+clear
+domain=$(cat /etc/xray/domain)
+tr="$(cat ~/log-install.txt | grep -w "Trojan WS" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+
+		read -rp "User: " -e user
+		user_EXISTS=$(grep -w $user /etc/xray/trojan.json | wc -l)
+
+		if [[ ${user_EXISTS} == '1' ]]; then
+clear
+		exit
+		fi
+	done
+
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#trojanws/a\#! '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/trojan.json
+sed -i '/#trojangrpc/a\#! '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/trojan.json
+
+trojanlink1="trojan://${uuid}@${domain}:${tr}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
+trojanlink="trojan://${uuid}@${domain}:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+clear
+cat > /home/vps/public_html/akun/${uuid}.txt << END
+"━━━━━━━━━━━━━━━━━━━━━"
+"VIP TROJAN ACCOUNT"
+"━━━━━━━━━━━━━━━━━━━━━"
+"Username: ${user}"
+"Expired: $exp"
+"Domain: ${domain}"
+"Port: ${tr}"
+"Key: ${uuid}"
+"Path: /trojan-ws"
+"Path GRPC: trojan-grpc"
+"━━━━━━━━━━━━━━━━━━━━━"
+"Link WS : ${trojanlink}"
+"━━━━━━━━━━━━━━━━━━━━━"
+"Link GRPC : ${trojanlink1}"
+"━━━━━━━━━━━━━━━━━━━━━"
+"THANKS FOR USING OUR SERVICE"
+END
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Terimakasih atas pembelian anda"
+echo -e "Link akun: https://${domain}:81/akun/${uuid}.txt"
+echo -e "Jika ada problem, hubungi admin @rumahvpn_admin"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+at now -f /root/restart-trojan.sh
