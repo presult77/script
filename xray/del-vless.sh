f520d461fce4eb2db82a8041759b04625b231a24
MasBro <presult77@gmail.com>
2023-12-25 14:49:00 +0700

    Update del-vless.sh

--- xray/del-vless.sh
+++ xray/del-vless.sh
@@ -8,6 +8,7 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/vless.json")
+total=$(($NUMBER_OF_CLIENTS / 2))
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "\E[44;1;39m     ⇱ Delete Vless Account ⇲     \E[0m"
@@ -25,7 +26,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/vless.json")
     echo "  User       Expired  " 
 	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	grep -E "^#& " "/etc/xray/vless.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[44;1;39m Total Vless User: $total \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     CLIENT_EXISTS=$(grep -w $user /etc/xray/vless.json | wc -l)
