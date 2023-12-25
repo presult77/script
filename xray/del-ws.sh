a930bcd20d7b644d33411909615b06a482c5cee2
MasBro <presult77@gmail.com>
2023-12-25 14:50:44 +0700

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -17,6 +17,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmess.json")
+total=$(($NUMBER_OF_CLIENTS / 2))
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "\E[44;1;39m     ⇱ Delete Vmess Account ⇲     \E[0m"
@@ -32,9 +33,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmess.json")
     echo -e "\E[44;1;39m     ⇱ Delete Vmess Account ⇲     \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo "  User       Expired  " 
-	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	grep -E "^### " "/etc/xray/vmess.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[44;1;39m Total Vmess User: $total \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     CLIENT_EXISTS=$(grep -w $user /etc/xray/vmess.json | wc -l)
