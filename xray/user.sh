25df031f3bbedafdcb2ab0665f39fa0152244b02
MasBro <presult77@gmail.com>
2023-12-25 15:02:29 +0700

    Update user.sh

--- xray/user.sh
+++ xray/user.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Menghitung jumlah akun untuk setiap jenis server
-trojan_accounts=$(grep -c -E "^#! " "/etc/xray/trojan.json")
-vmess_accounts=$(grep -c -E "^### " "/etc/xray/vmess.json")
-vless_accounts=$(grep -c -E "^#& " "/etc/xray/vless.json")
+trojan_count=$(grep -c -E "^#! " "/etc/xray/trojan.json")
+trojan_accounts=$((trojan_count / 2))
+vmess_count=$(grep -c -E "^### " "/etc/xray/vmess.json")
+vmess_accounts=$((vmess_count / 2))
+vless_count=$(grep -c -E "^#& " "/etc/xray/vless.json")
+vless_accounts=$((vless_count / 2))
 
 # Menghitung total akun
 total_accounts=$((trojan_accounts + vmess_accounts + vless_accounts))
